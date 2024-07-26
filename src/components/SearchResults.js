79f47d9776a4b22563264b26c583604889781700
aditya90205 <adityasrivastav90205@gmail.com>
2024-07-26 15:46:36 +0530

    Add ThemeContext and implement dark mode functionality

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
 
-const SearchResults = ({results}) => {
+const SearchResults = ({ results }) => {
+  const { darkMode } = useContext(ThemeContext);
+
   return (
-    <ul className='absolute top-12 border-2 w-full rounded-md h-64 overflow-y-scroll bg-white border-neutral-200 custom-scrollbar'>
-        {results.map((result) => (
-            <li key={result.symbol} className='p-4 m-2 flex items-center justify-between rounded-md hover:bg-indigo-200 cursor-pointer'>
-                <span>{result.symbol}</span>
-                <span>{result.description}</span>
-            </li>
-        ))}
+    <ul
+      className={`absolute top-12 border-2 w-full rounded-md h-64 overflow-y-scroll ${
+        darkMode
+          ? "bg-gray-900 border-gray-800 custom-scrollbar custom-scrollbar-dark"
+          : "bg-white border-neutral-200 custom-scrollbar"
+      }`}
+    >
+      {results.map((result) => (
+        <li
+          key={result.symbol}
+          className={`cursor-pointer p-4 m-2 flex items-center justify-between rounded-md ${
+            darkMode ? "hover:bg-indigo-600" : "hover:bg-indigo-200 "
+          } transition duration-300`}
+        >
+          <span>{result.symbol}</span>
+          <span>{result.description}</span>
+        </li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default SearchResults
+export default SearchResults;
--- src/components/ThemeIcon.js
+++ src/components/ThemeIcon.js
@@ -1,21 +1,27 @@
-import React from "react";
-// import ThemeContext from "../context/ThemeContext";
+import React, { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
 import { MoonIcon } from "@heroicons/react/solid";
 
 const ThemeIcon = () => {
-//   const { darkMode, setDarkMode } = useContext(ThemeContext);
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
 
-//   const toggleDarkMode = () => {
-    // setDarkMode(!darkMode);
-//   };
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
     <button
-    //   onClick={toggleDarkMode}
-      className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg`}
+      onClick={toggleDarkMode}
+      className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg ${
+        darkMode ? "shadow-gray-800" : null
+      }`}
     >
       <MoonIcon
-        className={`h-8 w-8 cursor-pointer stroke-1 fill-none stroke-neutral-400`}
+        className={`h-8 w-8 cursor-pointer stroke-1 fill-none stroke-neutral-400 ${
+            darkMode
+              ? "fill-yellow-400 stroke-yellow-400"
+              : "fill-none stroke-neutral-400"
+          }`}
       />
     </button>
   );
